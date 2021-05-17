bab39d4985846a53ceaf05b205dc06efc600e895
Dmitriy Litovchenko <msk.roytop@gmail.com>
2021-05-17 10:29:29 +0300

    Added more round functions

--- src/Numeric.php
+++ src/Numeric.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Bobel\Helpers;
 
 /**
@@ -72,5 +73,53 @@ class Numeric
         }
 
         return $int_num + $fraction_num;
+    }
+
+    /**
+     * Round up to specified number of decimal places
+     * @param float $float The number to round up
+     * @param int $dec How many decimals
+     */
+    public static function roundCeil(float $float, int $dec = 2)
+    {
+        if ($dec == 0) {
+            if ($float < 0) {
+                return floor($float);
+            } else {
+                return ceil($float);
+            }
+        } else {
+            $d = pow(10, $dec);
+
+            if ($float < 0) {
+                return floor($float * $d) / $d;
+            } else {
+                return ceil($float * $d) / $d;
+            }
+        }
+    }
+
+    /**
+     * Round down to specified number of decimal places
+     * @param float $float The number to round down
+     * @param int $dec How many decimals
+     */
+    function roundFloor(float $float, int $dec = 2)
+    {
+        if ($dec == 0) {
+            if ($float < 0) {
+                return ceil($float);
+            } else {
+                return floor($float);
+            }
+        } else {
+            $d = pow(10, $dec);
+
+            if ($float < 0) {
+                return ceil($float * $d) / $d;
+            } else {
+                return floor($float * $d) / $d;
+            }
+        }
     }
 }
